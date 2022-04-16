bc2cfe7964340bb54e9d10b36e6eefb811d1620f
Stefan Heiden <stefanheiden@gmail.com>
2022-04-16 09:33:42 +0200

    clean up

--- cmd/engine/model/board_test.go
+++ cmd/engine/model/board_test.go
@@ -214,35 +214,6 @@ func TestIllegalMovePieceTo(t *testing.T) {
 	}
 }
 
-func TestMovePieceTo(t *testing.T) {
-	testCasesRaw := `
-# WP A2 A3
-`
-	tests := GenerateTestCases(testCasesRaw, NewBoard())
-
-	for _, tt := range tests {
-		t.Run(tt.Name, func(t *testing.T) {
-
-			_, err := tt.Board.MovePiece(tt.Move)
-			wanted, _ := NewBoardFromString(wantedBoard)
-
-			if err != nil {
-				me, ok := err.(*MoveError)
-				if ok {
-					t.Errorf("%s \n", me.Err.Error())
-				}
-			}
-
-			if !reflect.DeepEqual(tt.Board.Fields, wanted.Fields) {
-				t.Errorf(" \n wanted: \n%s \n got: \n%s \n",
-					wanted.Print(nil),
-					tt.Board.Print(nil))
-			}
-		})
-	}
-
-}
-
 func TestGetPieceAtPos(t *testing.T) {
 
 	tests := []struct {
@@ -290,16 +261,3 @@ func TestGetPieceAtPos(t *testing.T) {
 		})
 	}
 }
-
-const wantedBoard = ` 
-    A   B   C   D   E   F   G   H  
-8 [BR][BN][BB][BQ][BK][BB][BN][BR] 8
-7 [BP][BP][BP][BP][BP][BP][BP][BP] 7
-6 [  ][  ][  ][  ][  ][  ][  ][  ] 6
-5 [  ][  ][  ][  ][  ][  ][  ][  ] 5
-4 [  ][  ][  ][  ][  ][  ][  ][  ] 4
-3 [WP][  ][  ][  ][  ][  ][  ][  ] 3
-2 [  ][WP][WP][WP][WP][WP][WP][WP] 2
-1 [WR][WN][WB][WQ][WK][WB][WN][WR] 1
-    A   B   C   D   E   F   G   H 
-`
