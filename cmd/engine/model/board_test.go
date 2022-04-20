84cb9fe9c4565890c3ac0c7ac980fc86e94177e1
Stefan Heiden <stefanheiden@gmail.com>
2022-04-20 16:32:27 +0200

    failing testcase to show failure in pawn move implementation

--- cmd/engine/model/board_test.go
+++ cmd/engine/model/board_test.go
@@ -7,144 +7,35 @@ import (
 	"testing"
 )
 
-func generate() []string {
-	return []string{
-		`
-NEW 
+func TestIllegalMoves(t *testing.T) {
+	counter := 0
+	testCaseRaws := generatIllegalMoves()
 
-# WP A2 A3
-# BP A7 A6
+	for _, testCaseRaw := range testCaseRaws {
 
-    A   B   C   D   E   F   G   H  
-8 [BR][BN][BB][BQ][BK][BB][BN][BR] 8
-7 [  ][BP][BP][BP][BP][BP][BP][BP] 7
-6 [BP][  ][  ][  ][  ][  ][  ][  ] 6
-5 [  ][  ][  ][  ][  ][  ][  ][  ] 5
-4 [  ][  ][  ][  ][  ][  ][  ][  ] 4
-3 [WP][  ][  ][  ][  ][  ][  ][  ] 3
-2 [  ][WP][WP][WP][WP][WP][WP][WP] 2
-1 [WR][WN][WB][WQ][WK][WB][WN][WR] 1
-    A   B   C   D   E   F   G   H 
-`,
+		testCase, err := generateMoveTestCase(testCaseRaw, counter)
+		counter++
 
-		`
-NEW 
-# BP A7 A6
+		t.Run(strconv.Itoa(testCase.Number), func(t *testing.T) {
+			if err != nil {
+				t.Errorf("test setup error %s", err)
+			}
 
-    A   B   C   D   E   F   G   H  
-8 [BR][BN][BB][BQ][BK][BB][BN][BR] 8
-7 [  ][BP][BP][BP][BP][BP][BP][BP] 7
-6 [BP][  ][  ][  ][  ][  ][  ][  ] 6
-5 [  ][  ][  ][  ][  ][  ][  ][  ] 5
-4 [  ][  ][  ][  ][  ][  ][  ][  ] 4
-3 [  ][  ][  ][  ][  ][  ][  ][  ] 3
-2 [WP][WP][WP][WP][WP][WP][WP][WP] 2
-1 [WR][WN][WB][WQ][WK][WB][WN][WR] 1
-    A   B   C   D   E   F   G   H 
-`,
+			for _, movement := range testCase.Moves {
 
-		`
-NEW 
-# BP A7 A6
-# WP A2 A4
-# BN B8 C6
-
-
-    A   B   C   D   E   F   G   H  
-8 [BR][  ][BB][BQ][BK][BB][BN][BR] 8
-7 [  ][BP][BP][BP][BP][BP][BP][BP] 7
-6 [BP][  ][BN][  ][  ][  ][  ][  ] 6
-5 [  ][  ][  ][  ][  ][  ][  ][  ] 5
-4 [WP][  ][  ][  ][  ][  ][  ][  ] 4
-3 [  ][  ][  ][  ][  ][  ][  ][  ] 3
-2 [  ][WP][WP][WP][WP][WP][WP][WP] 2
-1 [WR][WN][WB][WQ][WK][WB][WN][WR] 1
-    A   B   C   D   E   F   G   H 
-`,
-
-		`
-    A   B   C   D   E   F   G   H  
-8 [  ][  ][  ][  ][  ][  ][  ][  ] 8
-7 [  ][  ][  ][BP][  ][  ][  ][  ] 7
-6 [  ][  ][  ][  ][  ][  ][  ][  ] 6
-5 [  ][  ][  ][  ][  ][  ][  ][  ] 5
-4 [  ][  ][  ][  ][  ][  ][  ][  ] 4
-3 [  ][  ][  ][  ][  ][  ][  ][  ] 3
-2 [  ][  ][  ][  ][  ][  ][  ][  ] 2
-1 [  ][  ][  ][  ][  ][  ][  ][  ] 1
-    A   B   C   D   E   F   G   H 
-
-# BP D7 D6
-		
-    A   B   C   D   E   F   G   H  
-8 [  ][  ][  ][  ][  ][  ][  ][  ] 8
-7 [  ][  ][  ][  ][  ][  ][  ][  ] 7
-6 [  ][  ][  ][BP][  ][  ][  ][  ] 6
-5 [  ][  ][  ][  ][  ][  ][  ][  ] 5
-4 [  ][  ][  ][  ][  ][  ][  ][  ] 4
-3 [  ][  ][  ][  ][  ][  ][  ][  ] 3
-2 [  ][  ][  ][  ][  ][  ][  ][  ] 2
-1 [  ][  ][  ][  ][  ][  ][  ][  ] 1
-    A   B   C   D   E   F   G   H 
-`,
-
-		`
-    A   B   C   D   E   F   G   H  
-8 [  ][  ][  ][  ][BB][  ][  ][  ] 8
-7 [  ][  ][  ][  ][  ][  ][  ][  ] 7
-6 [  ][  ][  ][  ][  ][  ][  ][  ] 6
-5 [  ][  ][  ][  ][  ][  ][  ][  ] 5
-4 [  ][  ][  ][  ][  ][  ][  ][  ] 4
-3 [  ][  ][  ][  ][  ][  ][  ][  ] 3
-2 [  ][  ][  ][  ][  ][  ][  ][  ] 2
-1 [  ][  ][  ][  ][  ][  ][  ][  ] 1
-    A   B   C   D   E   F   G   H 
-
-# BB E8 D7
-		
-    A   B   C   D   E   F   G   H  
-8 [  ][  ][  ][  ][  ][  ][  ][  ] 8
-7 [  ][  ][  ][BB][  ][  ][  ][  ] 7
-6 [  ][  ][  ][  ][  ][  ][  ][  ] 6
-5 [  ][  ][  ][  ][  ][  ][  ][  ] 5
-4 [  ][  ][  ][  ][  ][  ][  ][  ] 4
-3 [  ][  ][  ][  ][  ][  ][  ][  ] 3
-2 [  ][  ][  ][  ][  ][  ][  ][  ] 2
-1 [  ][  ][  ][  ][  ][  ][  ][  ] 1
-    A   B   C   D   E   F   G   H 
-`,
-
-		`
-    A   B   C   D   E   F   G   H  
-8 [  ][  ][  ][BQ][  ][  ][  ][  ] 8
-7 [  ][  ][  ][  ][  ][  ][  ][  ] 7
-6 [  ][  ][  ][  ][  ][  ][  ][  ] 6
-5 [  ][  ][  ][  ][  ][  ][  ][  ] 5
-4 [  ][  ][  ][  ][  ][  ][  ][  ] 4
-3 [  ][  ][  ][  ][  ][  ][  ][  ] 3
-2 [  ][  ][  ][  ][  ][  ][  ][  ] 2
-1 [  ][  ][  ][  ][  ][  ][  ][  ] 1
-    A   B   C   D   E   F   G   H 
-
-# BQ D8 D7
-		
-    A   B   C   D   E   F   G   H  
-8 [  ][  ][  ][  ][  ][  ][  ][  ] 8
-7 [  ][  ][  ][BQ][  ][  ][  ][  ] 7
-6 [  ][  ][  ][  ][  ][  ][  ][  ] 6
-5 [  ][  ][  ][  ][  ][  ][  ][  ] 5
-4 [  ][  ][  ][  ][  ][  ][  ][  ] 4
-3 [  ][  ][  ][  ][  ][  ][  ][  ] 3
-2 [  ][  ][  ][  ][  ][  ][  ][  ] 2
-1 [  ][  ][  ][  ][  ][  ][  ][  ] 1
-    A   B   C   D   E   F   G   H 
-`,
+				_, err := testCase.InitialBoard.MovePiece(movement)
+				if err == nil {
+					t.Errorf("wanted error got none")
+				}
+			}
+		})
 	}
+
 }
 
 func TestLegalMoves(t *testing.T) {
 	counter := 0
-	testCaseRaws := generate()
+	testCaseRaws := generatLegalMoves()
 
 	for _, testCaseRaw := range testCaseRaws {
 
@@ -163,11 +54,13 @@ func TestLegalMoves(t *testing.T) {
 				}
 			}
 
-			if !reflect.DeepEqual(testCase.InitialBoard.Fields, testCase.WantedBoard.Fields) {
+			if !reflect.DeepEqual(testCase.InitialBoard.Fields,
+				testCase.WantedBoard.Fields) {
 				t.Errorf(" \n wanted: \n%s \n got: \n%s \n",
 					testCase.WantedBoard.Print(nil),
 					testCase.InitialBoard.Print(nil))
 			}
+
 		})
 	}
 }
